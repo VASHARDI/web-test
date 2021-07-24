405740601d0655d2b2039d945d75d19b5e75f80d
VASHARDI <78363709+VASHARDI@users.noreply.github.com>
2021-07-24 18:59:31 +0700

    1

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -159,7 +159,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = "/var/www/example.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, '..django/contrib/staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
